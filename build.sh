c6f0db5ecb5c038e0f0db4626393a1fa352bb7ad
Seth <seth@elypia.com>
2019-05-08 00:14:47 +0200

    adding script to export emotes in various sizes

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+mkdir output/
+cd emotes/
+
+for s in 256 128 64 32
+do
+    mkdir ../output/${s}x${s}
+    
+    for f in ./*
+    do
+        convert "./${f}" -resize ${s}x${s} "../output/${s}x${s}/${f}";
+    done
+done
