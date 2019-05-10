0d1daf7d4270d1674bfdb163ea638aa55437b45a
Seth Falco <seth@falco.fun>
2019-05-10 23:08:23 +0200

    Now using case to determine variable arguments rather than copy/paste

--- pages.sh
+++ pages.sh
@@ -4,27 +4,24 @@ if [ -d public/ ]; then rm -r public/; fi
 mkdir public/
 zip -r public/emotes.zip output/ 
 
-montage                                 \
-        -background none                \
-        -geometry +2+2                  \
-        -tile 8x                        \
-    output/*/128px/pandaAww.png         \
-        -gravity north                  \
-        -extent 128x144                 \
-        -gravity south                  \
-        -fill '#0008'                   \
-        -draw 'rectangle 0,128,144,144' \
-        -fill white                     \
-        -pointsize 14                   \
-        -font DejaVu-LGC-Sans           \
-        -annotate +0+0 %t               \
-    public/colors.png
+for i in 0 1
+do
+    case ${i} in
+        0)
+            glob="*/128px/pandaAww.png"
+            output="colors"
+            ;;
+        1)
+            glob="red/128px/*"
+            output="emotes"
+            ;; 
+    esac
 
-montage                                 \
+    montage                             \
         -background none                \
         -geometry +2+2                  \
         -tile 8x                        \
-    output/red/128px/*                  \
+    output/${glob}                      \
         -gravity north                  \
         -extent 128x144                 \
         -gravity south                  \
@@ -32,6 +29,6 @@ montage                                 \
         -draw 'rectangle 0,128,144,144' \
         -fill white                     \
         -pointsize 14                   \
-        -font DejaVu-LGC-Sans           \
         -annotate +0+0 %t               \
-    public/emotes.png
+    public/${output}.png
+done
