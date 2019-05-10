7d22c00016e932d48e2715c299751a44e29a16a0
Seth <seth@elypia.com>
2019-05-10 21:22:00 +0200

    Test

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM alpine:3.9.3
-RUN adduser -Sh /home/imagemagick imagemagick                      && \
-    apk add --no-cache --update imagemagick=7.0.8.44-r0 zip=3.0-r7
+RUN adduser -Sh /home/imagemagick imagemagick && \
+    apk add --no-cache --update                  \
+        imagemagick=7.0.8.44-r0                  \      
+        ttf-dejavu=2.35-r0                       \
+        zip=3.0-r7
 USER imagemagick
 WORKDIR /home/imagemagick
--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 if [ -d output/ ]; then rm -r output/; fi
-if [ -d public/ ]; then rm -r public/; fi
 
 for file in emotes/*
 do
@@ -42,37 +41,3 @@ do
         done
     done
 done
-
-mkdir public/
-zip -r public/emotes.zip output/ 
-
-montage                              \
-        -background none             \
-        -geometry +0+1               \
-        -tile 8x                     \
-    output/*/64px/pandaAww.png       \
-        -gravity north               \
-        -extent 74x74                \
-        -gravity south               \
-        -fill '#0008'                \
-        -draw 'rectangle 0,64,77,77' \
-        -fill white                  \
-        -pointsize 9                 \
-        -annotate +0+0 %t            \
-    public/colors.png
-
-montage                              \
-        -background none             \
-        -geometry +0+1               \
-        -tile 8x                     \
-    output/red/64px/*                \
-        -gravity north               \
-        -extent 74x74                \
-        -gravity south               \
-        -fill '#0008'                \
-        -draw 'rectangle 0,64,77,77' \
-        -fill white                  \
-        -pointsize 9                 \
-        -annotate +0+0 %t            \
-    public/emotes.png
-    
