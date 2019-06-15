b7c152039b49575ba457cbad003caa09eb9b5db0
Seth <seth@elypia.com>
2019-06-15 17:00:58 +0200

    * divided ci into test/build/deploy stage, and split script up further

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 if [ -d output/ ]; then rm -rf output/*; fi
-if [ -d public/ ]; then rm -rf public/*; fi
 
 for file in emotes/*
 do
@@ -16,10 +15,3 @@ do
     sh scripts/export.sh "pink"   "p" "100,100,66.6" ${file} ${emote} ${mask}
     sh scripts/export.sh "yellow" "y" "100,115,115"  ${file} ${emote} ${mask}
 done
-
-mkdir -p public/
-zip -rq public/emotes.zip output/ 
-
-# Create montages by using a glob and name of the montage output
-sh scripts/montage.sh "*/128px/*pandaAww.png" "colors"
-sh scripts/montage.sh "red/128px/*"           "emotes"
