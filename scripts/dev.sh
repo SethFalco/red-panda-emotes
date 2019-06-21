9b48f9dcbf837c25a056fcea026b0fe184c94bc8
Seth Falco <seth@falco.fun>
2019-06-21 20:47:23 +0200

    Updated dev scripts to use latest latest, where the build image will be kept.

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-tag=${1}
-
-if [ -z "${tag}" ]
-then 
-    tag="/imagemagick" 
-fi
-
-docker run -v $(pwd):/home/imagemagick               \
-    "registry.gitlab.com/elypia/elypia-emotes${tag}" \
-    /bin/sh                                          \
-        -c "bash scripts/test.sh                  && \
-            bash scripts/build.sh                 && \
+docker run -v $(pwd):/home/imagemagick             \
+    "registry.gitlab.com/elypia/elypia-emotes${1}" \
+    /bin/sh                                        \
+        -c "bash scripts/test.sh                && \
+            bash scripts/build.sh               && \
             sh scripts/pages.sh"
 
 # This is for testing images, it defaults to the latest
-# ImageMagick image, however can be overridden by specifying
+# image, however can be overridden by specifying
 # an image name as the first parameter.
