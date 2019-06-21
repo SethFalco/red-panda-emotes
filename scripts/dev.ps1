9b98a8669ba5f04e6398ca5900e47f5f3f75cdf5
Seth <seth@elypia.com>
2019-06-21 17:15:21 +0200

    Replaced `test` to use `bash` instead of `sh`

--- scripts/dev.ps1
+++ scripts/dev.ps1
@@ -2,7 +2,7 @@ ${tag} = "registry.gitlab.com/elypia/elypia-emotes/imagemagick"
 
 docker build -t ${tag} .
 
-docker run -v ${pwd}:/home/imagemagick                                                                           `
-    ${tag}                                                                                                       `
-    /bin/sh                                                                                                      `
-        -c "sed -i 's/\r//g' scripts/*.sh && sh scripts/test.sh && bash scripts/build.sh && sh scripts/pages.sh"
+docker run -v ${pwd}:/home/imagemagick                                                                             `
+    ${tag}                                                                                                         `
+    /bin/sh                                                                                                        `
+        -c "sed -i 's/\r//g' scripts/*.sh && bash scripts/test.sh && bash scripts/build.sh && sh scripts/pages.sh"
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -4,10 +4,10 @@ tag="registry.gitlab.com/elypia/elypia-emotes/imagemagick"
 # ${1} to specify arguments like --no-cache
 cat Dockerfile | docker build ${1} -t ${tag} -
 
-docker run -v `pwd`:/home/imagemagick                                           \
-    ${tag}                                                                      \
-    /bin/sh                                                                     \
-        -c "sh scripts/test.sh && bash scripts/build.sh && sh scripts/pages.sh"
+docker run -v `pwd`:/home/imagemagick                                             \
+    ${tag}                                                                        \
+    /bin/sh                                                                       \
+        -c "bash scripts/test.sh && bash scripts/build.sh && sh scripts/pages.sh"
 
 # If ${2} is true, push the image to GitLab.
 if [ "${2}" = "true" ]; then
