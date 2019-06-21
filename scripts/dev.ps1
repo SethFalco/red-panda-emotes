c92085447c45eb7f90b42f588ecb15949de25eaf
Seth <seth@elypia.com>
2019-06-21 20:47:23 +0200

    Updated dev scripts to use latest latest, where the build image will be kept.

--- scripts/dev.ps1
+++ scripts/dev.ps1
@@ -1,10 +1,11 @@
-docker run -v ${pwd}:/home/imagemagick                     `
-    "registry.gitlab.com/elypia/elypia-emotes/imagemagick" `
-    /bin/sh                                                `
-        -c "sed -i 's/\r//g' scripts/*.sh               && `
-            bash scripts/test.sh                        && `
-            bash scripts/build.sh                       && `
+docker run -v ${pwd}:/home/imagemagick         `
+    "registry.gitlab.com/elypia/elypia-emotes" `
+    /bin/sh                                    `
+        -c "sed -i 's/\r//g' scripts/*.sh   && `
+            bash scripts/test.sh            && `
+            bash scripts/build.sh           && `
             sh scripts/pages.sh"
 
-# This will just used the repositories latest ImageMagick image
-# only, and can not do anything else.
+# This will just used the repositories latest image
+# only which is considered a stable image
+# ready to build on. This and can not do anything else.
