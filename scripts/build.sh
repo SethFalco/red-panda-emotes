0cbdfb2807b356d5032daadcaadc3b9b46b0b47d
Seth Falco <seth@falco.fun>
2019-07-16 22:35:19 +0200

    Added violet panda

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,9 +7,10 @@ colors[0,0]="red";    colors[0,1]="";  colors[0,2]="100,100,100";
 colors[1,0]="blue";   colors[1,1]="b"; colors[1,2]="100,100,0";
 colors[2,0]="green";  colors[2,1]="g"; colors[2,2]="100,100,166";
 colors[3,0]="pink";   colors[3,1]="p"; colors[3,2]="100,100,66.6";
-colors[4,0]="yellow"; colors[4,1]="y"; colors[4,2]="100,115,115";
+colors[4,0]="violet"; colors[4,1]="v"; colors[4,2]="100,70,50";
+colors[5,0]="yellow"; colors[5,1]="y"; colors[5,2]="100,115,115";
 
-for c in 0 1 2 3 4
+for c in 0 1 2 3 4 5
 do
     export_arg="${export_arg} ( +clone -modulate ${colors[${c},2]} +clip-mask"
 
