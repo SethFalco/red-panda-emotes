f7bf9443e6f148134ed507cd113bc5453d72e3c2
Seth <seth@elypia.com>
2019-07-29 23:32:19 +0200

    Added red emotes.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,14 +3,15 @@ if [ -d output/ ]; then rm -rf output/*; fi
 
 # Append here for colors (Color | Prefix | Hue)
 declare -A colors
-colors[0,0]="red";    colors[0,1]="";  colors[0,2]="100,100,100";
+colors[0,0]="src";    colors[0,1]="";  colors[0,2]="100,100,100";
 colors[1,0]="blue";   colors[1,1]="b"; colors[1,2]="100,100,0";
 colors[2,0]="green";  colors[2,1]="g"; colors[2,2]="100,100,166";
 colors[3,0]="pink";   colors[3,1]="p"; colors[3,2]="100,100,66.6";
-colors[4,0]="violet"; colors[4,1]="v"; colors[4,2]="100,70,50";
-colors[5,0]="yellow"; colors[5,1]="y"; colors[5,2]="100,115,115";
+colors[4,0]="red";     colors[4,1]="r"; colors[4,2]="100,90,80";
+colors[5,0]="violet"; colors[5,1]="v"; colors[5,2]="100,70,50";
+colors[6,0]="yellow"; colors[6,1]="y"; colors[6,2]="100,115,115";
 
-for c in 0 1 2 3 4 5
+for c in 0 1 2 3 4 5 6
 do
     export_arg="${export_arg} ( +clone -modulate ${colors[${c},2]} +clip-mask"
 
