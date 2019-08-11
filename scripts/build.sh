e3d9a6162fb792330fd0d859190ed5afd0939ee7
Seth Falco <seth@falco.fun>
2019-08-11 06:59:16 +0200

    Another experimental to optimise the build speed but with the normal image.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,52 +1,21 @@
 #!/bin/bash
-if [ -d output/ ]; then rm -rf output/*; fi
 
-# Append here for colors (Color | Prefix | Hue)
-declare -A colors
-colors[0,0]="src";    colors[0,1]="";  colors[0,2]="100,100,100";
-colors[1,0]="blue";   colors[1,1]="b"; colors[1,2]="100,100,0";
-colors[2,0]="green";  colors[2,1]="g"; colors[2,2]="100,100,166";
-colors[3,0]="pink";   colors[3,1]="p"; colors[3,2]="100,100,66.6";
-colors[4,0]="red";    colors[4,1]="r"; colors[4,2]="100,90,79";
-colors[5,0]="violet"; colors[5,1]="v"; colors[5,2]="100,70,50";
-colors[6,0]="yellow"; colors[6,1]="y"; colors[6,2]="100,115,115";
-
-for c in 0 1 2 3 4 5 6
-do
-    export_arg="${export_arg} ( +clone -modulate ${colors[${c},2]} +clip-mask"
+# Build a giant string that represents the command we want to perform to build all emotes.
+# This is done so each input is only loaded once to general all exports.
+addcolor() { # COLOR, PREFIX, HUE
+    EXPORT_ARG="${EXPORT_ARG} ( +clone -modulate ${2} +clip-mask"
 
     # When adding a new resolution, just append the output size here.
-    for size in 512 258 128 112 72 64 56 36 28 18
+    for SIZE in 512 258 128 112 72 64 56 36 28 18
     do
-        mkdir -p output/${colors[${c},0]}/${size}px/
-        export_arg="${export_arg} ( +clone -resize ${size}X${size} -write output/${colors[${c},0]}/${size}px/${colors[${c},1]}[EMOTE] +delete )"
+        mkdir -p output/${0}/${SIZE}px/
+        export_arg="${EXPORT_ARG} ( +clone -resize ${SIZE}X${SIZE} -write output/${0}/${SIZE}px/${1}[EMOTE] +delete )"
     done
 
-    export_arg="${export_arg} +delete )"
-done
+    EXPORT_ARG="${EXPORT_ARG} +delete )"
+}
 
-for file in emotes/*
-do
-    emote=${file#*emotes/}
-    mask="masks/${emote/\.png/Mask.png}"
-
-    if [ -f ${mask} ]; then
-        mask_arg="-clip-mask ${mask}"
-    else
-        unset mask_arg
-    fi
-
-    echo "Exporting: ${file}"
-
-    emote_export_arg=${export_arg//\[EMOTE\]/${emote}}
-    magick ${file} -filter Catrom ${mask_arg} ${emote_export_arg} NULL:
-done
-
-# Make a public folder, things built here will get published on GitLab pages.
-if [ -d public/ ]; then rm -rf public/*; fi
-mkdir public
-
-exportmontage() {
+exportmontage() { # GLOB, FILENAME
     montage                             \
         -background none                \
         -geometry +2+2                  \
@@ -64,8 +33,43 @@ exportmontage() {
     public/${2}.png
 }
 
+# Append here for colors (Color | Prefix | Hue)
+addcolor "src",    "",  "100,100,100"
+addcolor "blue",   "b", "100,100,0"
+addcolor "green",  "g", "100,100,166"
+addcolor "pink",   "p", "100,100,66.6"
+addcolor "red",    "r", "100,90,79"
+addcolor "violet", "v", "100,70,50"
+addcolor "yellow", "y", "100,115,115"
+
+# Delete the output folders contents if it exists.
+if [ -d output/ ]; then rm -rf output/*; fi
+
+# Iterate all emotes and export them in all colors and sizes.
+for FILE in emotes/*
+do
+    EMOTE=${FILE#*emotes/}
+    MASK="masks/${EMOTE/\.png/Mask.png}"
+
+    if [ -f ${MASK} ]; then
+        MASK_ARG="-clip-mask ${MASK}"
+    else
+        unset MASK_ARG
+    fi
+
+    echo "Exporting: ${FILE}"
+
+    EMOTE_EXPORT_ARG=${EXPORT_ARG//\[EMOTE\]/${EMOTE}}
+    magick ${FILE} -filter Catrom ${MASK_ARG} ${EMOTE_EXPORT_ARG} NULL:
+done
+
+# Make a public folder, things built here will get published on GitLab pages.
+if [ -d public/ ]; then rm -rf public/*; fi
+mkdir public
+
+# Zip up all of the exported emotes into the public folder.
+zip -rq public/emotes.zip output/ LICENSE
+
 # Create montages by using a glob and name of the montage output
 exportmontage "*/128px/*pandaAww.png" "colors"
 exportmontage "src/128px/*"           "emotes"
-
-zip -rq public/emotes.zip output/ LICENSE
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -3,8 +3,7 @@ docker run -v $(pwd):/home/imagemagick             \
     "registry.gitlab.com/elypia/elypia-emotes${1}" \
     /bin/sh                                        \
         -c "bash scripts/test.sh                && \
-            bash scripts/build.sh               && \
-            sh scripts/pages.sh"
+            bash scripts/build.sh"
 
 # This is for testing images, it defaults to the latest
 # image, however can be overridden by specifying
