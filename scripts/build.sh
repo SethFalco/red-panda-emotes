cc1287bfc89bae8046a63a68990137ccba9061d1
Seth <seth@elypia.com>
2019-08-11 07:34:36 +0200

    Deleted useless files, reverted dockerfile for now, fixed bugs with new script (yet to test), removed one set of artifacts, moved project files.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,13 +3,13 @@
 # Build a giant string that represents the command we want to perform to build all emotes.
 # This is done so each input is only loaded once to general all exports.
 addcolor() { # COLOR, PREFIX, HUE
-    EXPORT_ARG="${EXPORT_ARG} ( +clone -modulate ${2} +clip-mask"
+    EXPORT_ARG="${EXPORT_ARG} ( +clone -modulate ${3} +clip-mask"
 
     # When adding a new resolution, just append the output size here.
     for SIZE in 512 258 128 112 72 64 56 36 28 18
     do
-        mkdir -p output/${0}/${SIZE}px/
-        export_arg="${EXPORT_ARG} ( +clone -resize ${SIZE}X${SIZE} -write output/${0}/${SIZE}px/${1}[EMOTE] +delete )"
+        mkdir -p output/${1}/${SIZE}px/
+        EXPORT_ARG="${EXPORT_ARG} ( +clone -resize ${SIZE}X${SIZE} -write output/${1}/${SIZE}px/${2}[EMOTE] +delete )"
     done
 
     EXPORT_ARG="${EXPORT_ARG} +delete )"
@@ -33,17 +33,17 @@ exportmontage() { # GLOB, FILENAME
     public/${2}.png
 }
 
-# Append here for colors (Color | Prefix | Hue)
-addcolor "src",    "",  "100,100,100"
-addcolor "blue",   "b", "100,100,0"
-addcolor "green",  "g", "100,100,166"
-addcolor "pink",   "p", "100,100,66.6"
-addcolor "red",    "r", "100,90,79"
-addcolor "violet", "v", "100,70,50"
-addcolor "yellow", "y", "100,115,115"
-
 # Delete the output folders contents if it exists.
 if [ -d output/ ]; then rm -rf output/*; fi
+
+# Append here for colors (Color | Prefix | Hue)
+addcolor "src"    ""  "100,100,100"
+addcolor "blue"   "b" "100,100,0"
+addcolor "green"  "g" "100,100,166"
+addcolor "pink"   "p" "100,100,66.6"
+addcolor "red"    "r" "100,90,79"
+addcolor "violet" "v" "100,70,50"
+addcolor "yellow" "y" "100,115,115"
 
 # Iterate all emotes and export them in all colors and sizes.
 for FILE in emotes/*
@@ -65,7 +65,7 @@ done
 
 # Make a public folder, things built here will get published on GitLab pages.
 if [ -d public/ ]; then rm -rf public/*; fi
-mkdir public
+mkdir -p public
 
 # Zip up all of the exported emotes into the public folder.
 zip -rq public/emotes.zip output/ LICENSE
