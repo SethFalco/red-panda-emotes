91b8833bf137795ea8f40fe4917a9b78fd55b04f
Seth <seth@elypia.com>
2019-08-14 04:39:15 +0200

    Deleted all script files and moved relevent commands inside gitlab.ci

--- scripts/dev.ps1
+++
@@ -1,9 +0,0 @@
-docker run -v ${pwd}:/home/imagemagick         `
-    "registry.gitlab.com/elypia/elypia-emotes" `
-    /bin/sh                                    `
-        -c "sed -i 's/\r//g' scripts/*.sh   && `
-            bash scripts/test.sh            && `
-            bash scripts/build.sh"
-
-# This will only use the repositories latest image which is considered a stable
-# and ready to build on, and won't do much else.
